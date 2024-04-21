0c7d9116fde9fcbf0eb4aebb58795204caf72c2a
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-21 18:19:27 +0530

    Added MongoDB Shells

--- create_ec2.sh
+++ create_ec2.sh
@@ -16,7 +16,7 @@ do
         INSTANCE_TYPE="t3.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "resourceType=Instance, Tags= [{Key=Name, Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags= [{Key=Name, Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
     echo "Creating Instance -> $i----PrivateIP : $IP_ADDRESS"
 
