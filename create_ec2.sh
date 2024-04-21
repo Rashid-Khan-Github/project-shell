100e3eb2c7e783c4e564ae3a250069a880cceb1d
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-21 18:23:10 +0530

    Added MongoDB Shells

--- create_ec2.sh
+++ create_ec2.sh
@@ -20,6 +20,18 @@ do
 
     echo "Creating Instance -> $i----PrivateIP : $IP_ADDRESS"
 
+    aws route53 change-resource-record-sets --hosted-zone-id Z0513646AB3WBDGO9A9V --change-batch '
+        {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "'$i.$DOMAIN_NAME'",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+        }
+    '
 
 done
 
